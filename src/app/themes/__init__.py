b45fa43e50931fbc151fecd3ac8b390d1c05336e
LeKSuS-04 <tarasovion2004@gmail.com>
2021-11-04 19:05:24 +0300

    Theme loading and AnimatedSprite classes

--- src/app/themes/__init__.py
+++ src/app/themes/__init__.py
@@ -1,17 +1,115 @@
 import os
 
 from main import resource_path
+from app.themes.sprite import *
+
 
 class Theme:
     PATH = "themes"
+    SPRITE_TYPES = {
+        "dot": SingleDirectionAnimatedSprite,
+        "enemy": FourDirectionAnimatedSprite,
+        "enemy_scare": FourDirectionAnimatedSprite,
+        "energizer": SingleDirectionAnimatedSprite,
+        "floor": SingleDirectionAnimatedSprite,
+        "fruit": SingleDirectionAnimatedSprite,
+        "ghost_box_exit": TwoDirectionAnimatedSprite,
+        "player": FourDirectionAnimatedSprite,
+        "top_corner": SingleDirectionAnimatedSprite,
+        "wall": TwoDirectionAnimatedSprite,
+    }
 
-    def __init__():
-        ...
+    def __init__(self, sprites: dict[str, list[AnimatedSprite]]):
+        self._sprites = sprites
 
-    def load_from_file():
-        ...
+    @classmethod
+    def _load_sprite_variations(cls, sprite_path, sprite_type) -> list[AnimatedSprite]:
+        variations = []
+
+        i = 0
+        variation_path = os.path.join(sprite_path, str(i))
+        while os.path.exists(variation_path):
+            variations.append(sprite_type.load(variation_path))
+
+            i += 1
+            variation_path = os.path.join(sprite_path, str(i))
+
+        if len(variations) == 0:
+            raise FileNotFoundError(
+                f"No variations of sprite were found inside {sprite_path}"
+            )
+
+        return variations
+
+    @classmethod
+    def load_theme(cls, theme_name: str) -> "Theme":
+        """Creates new theme object
+
+        Args:
+            theme_name (str): Theme name. List of available themes can be get
+            from Theme.get_available
+        """
+
+        theme_root = resource_path(os.path.join(Theme.PATH, theme_name))
+        if not os.path.exists(theme_root):
+            raise FileNotFoundError(f'Theme "{theme_name}" cannot be found')
+
+        sprites = {}
+        for sprite_type in Theme.SPRITE_TYPES:
+            sprite_path = os.path.join(theme_root, sprite_type)
+            if not os.path.exists(sprite_path):
+                raise FileNotFoundError(
+                    f'Sprite "{sprite_type}" doesn\'t exist in theme "{theme_name}"'
+                )
+
+            variations = Theme._load_sprite_variations(sprite_path, 
+                                                       Theme.SPRITE_TYPES[sprite_type])
+            sprites[sprite_type] = variations
+
+        return cls(sprites)
 
     @staticmethod
     def get_available() -> list[str]:
-        """ Returns list of themes from /themes folder """
+        """Returns list of themes from /themes folder"""
+
         return os.listdir(resource_path(Theme.PATH))
+
+    @property
+    def dot(self) -> list[AnimatedSprite]:
+        return self._sprites.get("dot")
+
+    @property
+    def enemy(self) -> list[AnimatedSprite]:
+        return self._sprites.get("enemy")
+
+    @property
+    def enemy_scare(self) -> list[AnimatedSprite]:
+        return self._sprites.get("enemy_scare")
+
+    @property
+    def energizer(self) -> list[AnimatedSprite]:
+        return self._sprites.get("energizer")
+
+    @property
+    def floor(self) -> list[AnimatedSprite]:
+        return self._sprites.get("floor")
+
+    @property
+    def fruit(self) -> list[AnimatedSprite]:
+        return self._sprites.get("fruit")
+
+    @property
+    def ghost_box_exists(self) -> list[AnimatedSprite]:
+        return self._sprites.get("ghost_box_exists")
+
+    @property
+    def player(self) -> list[AnimatedSprite]:
+        return self._sprites.get("player")
+
+    @property
+    def top_corner(self) -> list[AnimatedSprite]:
+        return self._sprites.get("top_corner")
+
+    @property
+    def wall(self) -> list[AnimatedSprite]:
+        return self._sprites.get("wall")
---
+++ src/app/themes/sprite.py
@@ -0,0 +1,175 @@
+import pygame
+import os
+
+from abc import ABC, abstractmethod
+
+from utilities.direction import Direction
+
+
+class AnimatedSprite(ABC):
+    def __init__(self):
+        self._amount_of_frames = 0
+        self._frame_idx = 0
+        
+    def next_frame(self) -> None:
+        self._frame_idx = (self._frame_idx + 1) % self._amount_of_frames
+
+    @staticmethod
+    def _load(sprite_path) -> list[pygame.Surface]:
+        if not os.path.exists(sprite_path):
+            raise FileNotFoundError(f"Path {sprite_path} doesn't exist")
+
+        frames = []
+        i = 0
+        frame_path = os.path.join(sprite_path, f"{i}.png")
+        while os.path.exists(frame_path):
+            img = pygame.image.load(frame_path)
+            frames.append(img)
+            
+            i += 1
+            frame_path = os.path.join(sprite_path, f"{i}.png")
+
+        if len(frames) == 0:
+            raise FileNotFoundError(
+                f"No _frames were found inside {sprite_path}"
+            )
+
+        return frames
+
+    @abstractmethod
+    def frame(self, change_frame=True):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def load(cls, sprite_path):
+        pass
+
+
+class SingleDirectionAnimatedSprite(AnimatedSprite):
+    """ Represents game sprite """
+
+    def __init__(self, frames: list[pygame.Surface]):
+        super().__init__()
+        self._frames = frames
+        self._amount_of_frames = len(frames)
+
+    def frame(self, change_frame=True) -> pygame.Surface:
+        """Returns current frame of sprite
+
+        Args:
+            change_frame (bool, optional): Indicates whether frame
+            should be changed. Defaults to True.
+
+        Returns:
+            pygame.Surface: surface containing current frame image
+        """
+
+        current_frame = self._frames[self._frame_idx]
+        if change_frame:
+            self.next_frame
+        return current_frame
+
+    @classmethod
+    def load(cls, sprite_path) -> "SingleDirectionAnimatedSprite":
+        frames = cls._load(sprite_path)
+        return cls(frames)
+
+
+class TwoDirectionAnimatedSprite(AnimatedSprite):
+    def __init__(self, frames: list[pygame.Surface]):
+        super().__init__()
+
+        self._frames = frames
+        self._rev_frames = [pygame.transform.flip(img, True, False)
+                            for img in frames]
+
+        self._amount_of_frames = len(frames)
+
+    def frame(self, reversed_: bool = False, 
+              change_frame: bool = True) -> pygame.Surface:
+        """Returns current frame of sprite
+
+        Args:
+            reversed (bool, optional): If True, returns inverted 
+            horizontally frame. Defaults to False.
+            change_frame (bool, optional): Indicates whether frame
+            should be changed. Defaults to True.
+
+        Returns:
+            pygame.Surface: surface containing current frame image
+        """
+
+        current_frame = None
+        if not reversed_:
+            current_frame = self._frames[self._frame_idx]
+        else:
+            current_frame = self._rev_frames[self._frame_idx]
+
+        if change_frame:
+            self.next_frame()
+
+        return current_frame
+
+    @classmethod
+    def load(cls, sprite_path) -> 'TwoDirectionAnimatedSprite':
+        frames = cls._load(sprite_path)
+        return cls(frames)
+
+
+class FourDirectionAnimatedSprite(AnimatedSprite):
+    def __init__(self, fwd_frames: list[pygame.Surface], 
+                 bwd_frames: list[pygame.Surface]):
+        super().__init__()
+
+        if (len(fwd_frames) != len(bwd_frames)):
+            raise ValueError("Amount of forward and backward frames doesn't match")
+        
+        self._W_frames = fwd_frames
+        self._S_frames = [pygame.transform.flip(img, True, False) 
+                              for img in fwd_frames]
+        self._N_frames = bwd_frames
+        self._E_frames = [pygame.transform.flip(img, True, False) 
+                              for img in bwd_frames]
+
+        self._amount_of_frames = len(fwd_frames)
+
+    def frame(self, direction: int,
+              change_frame: bool=True) -> pygame.Surface:
+        """Returns current frame of sprite
+
+        Args:
+            direction(int): Orientation of sprite
+            change_frame (bool, optional): Indicates whether frame
+            should be changed. Defaults to True.
+
+        Returns:
+            pygame.Surface: surface containing current frame image
+        """
+
+        current_frame = None
+        if direction == Direction.N:
+            current_frame = self._N_frames[self._frame_idx]
+        elif direction == Direction.E:
+            current_frame = self._E_frames[self._frame_idx]
+        elif direction == Direction.S:
+            current_frame = self._S_frames[self._frame_idx]
+        elif direction == Direction.W:
+            current_frame = self._W_frames[self._frame_idx]
+        else:
+            raise ValueError(f'Invalid direction value: {direction}')
+
+        if change_frame:
+            self.next_frame()
+
+        return current_frame
+
+    @classmethod
+    def load(cls, sprite_path) -> 'FourDirectionAnimatedSprite':
+        fwd_path = os.path.join(sprite_path, 'forward')
+        fwd_frames = cls._load(fwd_path)
+
+        bwd_path = os.path.join(sprite_path, 'backward')
+        bwd_frames = cls._load(bwd_path)
+
+        return cls(fwd_frames, bwd_frames)
